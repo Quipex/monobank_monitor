ffa91283da4799462572ec4b8bc167fd20f29dce
Quipex <rudas72@gmail.com>
2021-02-28 18:57:33 +0200

    fix for account

--- src/monobank/model/WebhookEvent.ts
+++ src/monobank/model/WebhookEvent.ts
@@ -1,14 +1,13 @@
-import { Account, accountToString } from './Account';
 import { Statement, statementToString } from './Statement';
 
 export interface WebhookEvent {
     type: string;
     data: {
-        account: Account;
+        account: string;
         statementItem: Statement;
     }
 }
 
 export const eventToString = ({ data: { account, statementItem } }: WebhookEvent) => (
-    `${accountToString(account)}\n${statementToString(statementItem)}`
+    `Карта '${account}'\n${statementToString(statementItem)}`
 )
