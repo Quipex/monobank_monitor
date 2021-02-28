694cbd9348c74865912939b27264d1bb2fe7ee0b
Quipex <rudas72@gmail.com>
2021-02-28 22:10:47 +0200

    Made kyiv as default local

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ import moment from "moment";
 import { launchServer } from "./src/server/server";
 
 moment.locale('ru');
+moment.tz.setDefault('Europe/Kiev');
 launchServer();
--- src/monobank/model/Statement.ts
+++ src/monobank/model/Statement.ts
@@ -1,4 +1,4 @@
-import moment from "moment";
+import moment from "moment-timezone";
 import { currency, toPrice as _toPrice } from "../../utils/money.helper";
 
 export interface Statement {
