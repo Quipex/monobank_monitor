9a067f9f6c4f9bf8ac935837222b1b9564061502
DmytroR <rudas72@gmail.com>
2023-05-25 18:34:16 +0300

    Read the first line of the .env when creating container

--- run_latest_docker.sh
+++ run_latest_docker.sh
@@ -1,2 +1,19 @@
-source .env
+#!/bin/bash
+
+# Specify the file path
+file_path="./.env"
+
+# Extract the first line of the file
+first_line=$(head -n 1 "$file_path")
+
+# Create a temporary file to store the first line
+tmp_file=$(mktemp)
+echo "$first_line" > "$tmp_file"
+
+# Source the temporary file
+source "$tmp_file"
+
+# Clean up the temporary file
+rm "$tmp_file"
+
 docker run --name monobot -dp $APP_PORT:$APP_PORT --env-file ./.env quipex/monobot:latest
