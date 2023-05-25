41a853ee83de594bc600492eee5ad1cf0f4675da
DmytroR <rudas72@gmail.com>
2023-05-25 17:04:09 +0300

    Show the commit meta

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
+import { readFileSync } from 'fs';
 import logger from "../logging/logger";
 import { errorHandler, successHandler } from '../logging/morgan';
 import { env } from '../utils/env';
@@ -7,6 +8,7 @@ import { answerToMonobankGet } from './features/answerToMonobankGet';
 import { handleNewPaymentEvent } from './features/handleNewPaymentEvent';
 
 const app = express();
+const commitMeta = readFileSync('commit_meta');
 
 const launchServer = () => {
     app.use(bodyParser.json());
@@ -17,7 +19,7 @@ const launchServer = () => {
     app.post('/monobank', handleNewPaymentEvent);
 
     app.listen(env.app.port, () => {
-        logger.info(`App is listening on port ${env.app.port}`);
+        logger.info(`App is listening on port ${env.app.port}\nLatest commit: ${commitMeta}`);
     });
 };
 
